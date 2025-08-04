df5338b9ee93dba449873b79584aa1611f594cf6
Dhruv <sheredhruv@gmail.com>
2025-08-04 11:15:22 +0530

    fix: update chatbot image source and correct favicon link for consistency

--- agrosence/components/Chatbot.jsx
+++ agrosence/components/Chatbot.jsx
@@ -105,7 +105,7 @@ const Chatbot = () => {
               >
                 {chat.sender === "bot" && (
                   <img
-                    src={resource.Chatbot.src}
+                    src={resource.Logo3.src}
                     alt={resource.Chatbot.alt}
                     className="chatbox-image border border-2 border-success"
                   />
